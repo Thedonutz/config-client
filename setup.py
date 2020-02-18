bd17c4e9e19c77bdac021837f7192c1a47399d26
Jeff Morgan <quicksh0t12@gmail.com>
2020-02-17 18:51:44 -0600

    update setup

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="config-client",
+    name="config-client-pcf",
     version="0.5.0",
-    author="alexandre menezes",
-    author_email="alexandre.fmenezes@gmail.com",
-    description="config service client",
+    author="alexandre menezes/jeff m",
+    author_email="quicksh0t12@gmail.com",
+    description="config service client for pcf config-server3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='Apache-2.0',
-    url="https://github.com/amenezes/config-client",
+    url="https://github.com/thedonutz/config-client",
     packages=setuptools.find_packages(),
     python_requires='>=3.6.0',
     project_urls=OrderedDict((
-        ('Documentation', 'https://github.com/amenezes/config-client'),
-        ('Code', 'https://github.com/amenezes/config-client'),
-        ('Issue tracker', 'https://github.com/amenezes/config-client/issues')
+        ('Documentation', 'https://github.com/thedonutz/config-client'),
+        ('Code', 'https://github.com/thedonutz/config-client'),
+        ('Issue tracker', 'https://github.com/thedonutz/config-client/issues')
     )),
     install_requires=[
         'attrs>=19.1.0',
